33420c17a2ce7b97c3f440083ee912fa806d8a52
ericsix-lacework <eric.six@lacework.net>
2022-03-31 19:45:53 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -7,7 +7,7 @@ from flask import Flask, render_template, url_for, request, jsonify
 
 app = Flask(__name__)
 
-app_name = 'Lacework and TikTok'
+app_name = 'Lacework for the Win' 
 
 print(app_name)
 

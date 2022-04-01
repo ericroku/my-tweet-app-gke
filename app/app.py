1131381d45f9b6ed1d211cbbf0d7657f7148e3bc
ericsix-lacework <eric.six@lacework.net>
2022-04-01 00:26:56 -0500

    Image deploy changes
    Validation of new image tag push

--- app/app.py
+++ app/app.py
@@ -7,7 +7,7 @@ from flask import Flask, render_template, url_for, request, jsonify
 
 app = Flask(__name__)
 
-app_name = 'Roadhouse rules' 
+app_name = 'Lacework Roadhouse Roundhouses' 
 
 print(app_name)
 

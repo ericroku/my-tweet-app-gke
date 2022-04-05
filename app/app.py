5075f91468972d62e15c62d05513523bfcf93657
ericsix-lacework <eric.six@lacework.net>
2022-04-05 12:01:17 -0500

    20220322 - Demo Updates

--- app/app.py
+++ app/app.py
@@ -7,7 +7,7 @@ from flask import Flask, render_template, url_for, request, jsonify
 
 app = Flask(__name__)
 
-app_name = 'Lacework Roadhouse Roundhouses' 
+app_name = 'The Lacework Team Thanks you for your time today' 
 
 print(app_name)
 

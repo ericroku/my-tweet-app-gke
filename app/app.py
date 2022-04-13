959c54c3d9203197deb424c0e7280f5116f7dc60
ericsix-lacework <eric.six@lacework.net>
2022-04-13 17:52:53 -0500

    20220413 - Demo Update

--- app/app.py
+++ app/app.py
@@ -18,7 +18,7 @@ def index():
         url_for('static', filename='beachops-1.png'),
         url_for('static', filename='beachops-2.png'),
         url_for('static', filename='norules-1.png'),
-        ul_for('static', filename='norules-2.png')
+        url_for('static', filename='norules-2.png')
     ]
     url = random.choice(images)
     hostname = platform.node()

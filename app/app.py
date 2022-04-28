9c4f5398ca69a80eca3d99fa50b2d455aa9666d9
ericsix-lacework <eric.six@lacework.net>
2022-04-28 15:04:41 -0500

    20220428 DEMO

--- app/app.py
+++ app/app.py
@@ -7,7 +7,7 @@ from flask import Flask, render_template, url_for, request, jsonify
 
 app = Flask(__name__)
 
-app_name = 'The Lacework Team thanks GM Financial for your time today' 
+app_name = 'The Lacework Team thanks you for your time today' 
 
 print(app_name)
 

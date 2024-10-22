72635ccaf0def83200ef714642b272ddde97c925
ericroku <ericsix@gmail.com>
2024-10-23 03:11:44 +0900

    Update app.py

--- app/app.py
+++ app/app.py
@@ -7,7 +7,7 @@ from flask import Flask, render_template, url_for, request, jsonify
 
 app = Flask(__name__)
 
-app_name = 'Rock n roll to the rescue' 
+app_name = 'Rock n roll to the rescue!!' 
 
 print(app_name)
 
